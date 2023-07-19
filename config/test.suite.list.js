ff176380542023322f7715c5b4899591d0f2240b
lynix28 <lynixizunia@gmail.com>
2023-07-20 01:21:58 +0700

    Redefine test config to isolate the run per file

--- config/test.suite.list.js
+++ config/test.suite.list.js
@@ -1,24 +1,14 @@
 const path = '../tests/';
 
 const testSuite = {
-	SmokeTest: [ // example to run it as a Test Suite
-		[
-			path + 'TS-001-Open-App.js',
-		]
-	],
+	SmokeTest: path + 'TS-001-Open-App.js',
 	SanityTest: [
-		[
-			path + 'TS-002-MainMenu.js',
-			path + 'TS-003-SideMenu.js',
-			path + 'TS-004-Webview.js',
-			path + 'TS-005-LoginLogout.js'
-		]
+		path + 'TS-002-MainMenu.js',
+		path + 'TS-003-SideMenu.js',
+		path + 'TS-004-Webview.js',
+		path + 'TS-005-LoginLogout.js'
 	],
-	NegativeTest: [
-		[
-			path + 'TS-006-NegativeCase.js'
-		]
-	]
+	NegativeTest: path + 'TS-006-NegativeCase.js'
 };
 
 module.exports = { testSuite };
