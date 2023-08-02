377099792ae3bc3cfd6fb952172874c4e7036f53
lynix28 <lynixizunia@gmail.com>
2023-08-02 23:40:29 +0700

    Update

--- config/wdio.conf.cucumber.cloud.js
+++ config/wdio.conf.cucumber.cloud.js
@@ -10,6 +10,8 @@ const sauceLabsCapabilities = {
 	'appium:deviceOrientation': 'portrait',
 	'appium:platformVersion': '13.0',
 	'appium:automationName': 'UiAutomator2',
+	'appium:autoLaunch': false,
+	'appium:fullReset': true,
 	'sauce:options': {
 		appiumVersion: '2.0.0-beta66',
 		build: `android.v1.${process.env.BUILD}`,
